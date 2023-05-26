e839d3046e020f6d60fcd265b31b8e955c3013ba
Donya <donyaelbanna@yahoo.com>
2023-05-26 23:48:39 +0300

    implemented guarded routes, edit user

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,11 +1,8 @@
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
-// import { SiteUsersService } from '../services/site-users.service';
-// import { map, catchError, of } from 'rxjs';
 
 export const authGuard = () => {
   const router = inject(Router);
-  // const authService = inject(SiteUsersService);
 
   const currentUser = localStorage.getItem('authed');
 
